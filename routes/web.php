aa3b4bf8371914ab5e46ad5ca69d0f6c92dc00ab
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-10-09 14:04:40 +0100

    Implemented Post View count and All Posts

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/posts', 'PostController@index')->name('post.index');
 
 Route::get('post/{slug}', 'PostController@details')->name('post.details');
 
